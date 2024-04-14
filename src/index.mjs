cad9659bca7acb5f80110354c415b52577aea318
Nicolas Soroka <nsoroka@quadiontech.com>
2024-04-14 18:17:12 -0300

    fc

---
+++ src/index.mjs
@@ -0,0 +1,77 @@
+import express from 'express'
+
+const app = express()
+
+app.use(express.json())
+
+const resolveIndexByUserId = (req, res, next) => {
+  const {
+    params: { id }
+  } = req
+  const parsedId = parseInt(id)
+  if (isNaN(parsedId)) return res.sendStatus(400)
+  const findUserIndex = mockUsers.findIndex(user => user.id === parsedId)
+  if (findUserIndex === -1) return res.sendStatus(404)
+  req.findUserIndex = findUserIndex
+  next()
+}
+
+const PORT = process.env.PORT || 3000
+
+const mockUsers = [
+  { id: 1, username: 'james', displayName: 'James' },
+  { id: 2, username: 'nick', displayName: 'Nico' },
+  { id: 3, username: 'pepe', displayName: 'Pepe' }
+]
+
+app.get('/', (req, res) => {
+  res.status(201).send({ msg: 'Hello' })
+})
+
+app.get('/api/users', (req, res) => {
+  const {
+    query: { filter, value }
+  } = req
+
+  if (filter && value) {
+    return res.send(mockUsers.filter(user => user[filter].includes(value)))
+  }
+  return res.send(mockUsers)
+})
+
+app.post('/api/users', (req, res) => {
+  console.log(req.body)
+  const { body } = req
+  const newUser = { id: mockUsers[mockUsers.length - 1].id + 1, ...body }
+  mockUsers.push(newUser)
+  return res.status(201).send(newUser)
+})
+
+app.get('/api/users/:id', resolveIndexByUserId, (req, res) => {
+  const { findUserIndex } = req
+  const findUser = mockUsers[findUserIndex]
+  if (!findUser) return res.sendStatus(404)
+  return res.send(findUser)
+})
+
+app.put('/api/users/:id', resolveIndexByUserId, (req, res) => {
+  const { body, findUserIndex } = req
+  mockUsers[findUserIndex] = { id: mockUsers[findUserIndex].id, ...body }
+  return res.sendStatus(200)
+})
+
+app.patch('/api/users/:id', resolveIndexByUserId, (req, res) => {
+  const { body, findUserIndex } = req
+  mockUsers[findUserIndex] = { ...mockUsers[findUserIndex], ...body }
+  return res.sendStatus(200)
+})
+
+app.delete('/api/users/:id', resolveIndexByUserId, (req, res) => {
+  const { findUserIndex } = req
+  mockUsers.splice(findUserIndex, 1)
+  return res.sendStatus(200)
+})
+
+app.listen(PORT, () => {
+  console.log(`Running on PORT ${PORT}`)
+})
